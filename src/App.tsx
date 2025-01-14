1d6ec0fff0aec6b09990c54c0d14737f8b1b1d82
RMP1992 <45944114+RMP1992@users.noreply.github.com>
2025-01-14 17:51:59 +0000

    Installed Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Button } from "@chakra-ui/react";
 function App() {
   return (
     <div className="App">
-      <Button colorScheme="blue">Hello</Button>
+      <Button colorScheme="teal" variant="solid">
+        Hello
+      </Button>
     </div>
   );
 }
